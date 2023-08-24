34bcd1a3cee8a22b63f4df35773bc1900de7767e
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-08-24 13:59:17 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[user,setUser] = useState({
+    name:"",
+    email:"",
+    password:""
+  })
+
+  function handleSubmit(e){
+      e.preventDefault()
+      if(!user.name || !user.email || !user.password){
+        alert("All fields are mandatory!")
+      }
+      console.log(user)
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={handleSubmit}>
+            <label name="userName">Name</label>
+            <input type="text" onChange={e => setUser({...user, name: e.target.value})} /> <br/>
+            <label name="email">Email</label>
+            <input type="email" onChange={e => setUser({...user, email: e.target.value})} /> <br/>
+            <label name="password">Password</label>
+            <input type="password" onChange={e => setUser({...user, password: e.target.value})} /> <br/>
+            <button type="submit">Submit</button>
+        </form>
     </div>
   )
 }
